5ac1f89eda404fcd88484c984f624898b02e82c4
Andrew Davison <260552+apdavison@users.noreply.github.com>
2012-01-13 14:37:40 +0100

    Initial copy from PyNN, plus packaging

---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='lazyarray',
+    version='0.1.0dev',
+    py_modules=['lazyarray'],
+    license='Modified BSD',
+    author="Andrew P. Davison",
+    author_email="andrew.davison@unic.cnrs-gif.fr",
+    url="http://bitbucket.org/apdavison/lazyarray/",
+    description="a Python package that provides a lazily-evaluated numerical array class, larray, based on and compatible with NumPy arrays.",
+    long_description=open('README').read(),
+    install_requires=[
+        "numpy >= 1.1"
+    ],   
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+    ]
+)
+
