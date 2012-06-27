7a3dc5547ca6d372c16ae092f34f68f12d01868d
Andrew Davison <260552+apdavison@users.noreply.github.com>
2012-06-27 10:52:48 +0200

    Previous release didn't work with Python 3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='lazyarray',
-    version='0.2.1dev',
+    version='0.2.1',
     py_modules=['lazyarray'],
     license='Modified BSD',
     author="Andrew P. Davison",
