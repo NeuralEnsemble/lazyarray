5ee96047f16654c19c0579a85d752b0e2d79a196
Andrew Davison <andrew.davison@cnrs.fr>
2024-09-05 16:57:56 +0200

    Switch to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,27 +1,4 @@
-# -*- coding: utf-8 -*-
+import setuptools
 
-from distutils.core import setup
-
-setup(
-    name='lazyarray',
-    version='0.5.3.dev',
-    py_modules=['lazyarray'],
-    license='Modified BSD',
-    author="Andrew P. Davison",
-    author_email="andrew.davison@cnrs.fr",
-    url="http://github.com/NeuralEnsemble/lazyarray/",
-    description="a Python package that provides a lazily-evaluated numerical array class, larray, based on and compatible with NumPy arrays.",
-    long_description=open('README.rst').read(),
-    install_requires=[
-        "numpy >= 1.13",
-    ],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Topic :: Scientific/Engineering',
-    ]
-)
+if __name__ == "__main__":
+    setuptools.setup()
